6cc40ae57dba5f9c85730439d3f18974c7e23f5e
PhongVan <fmlphongvan@gmail.com>
2022-10-25 10:58:20 +0700

    Update AI for car

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour {
+    public Color laserColor = new Color(0, 255, 0, 0.5f);
+    public float distanceLaser = 50;
+    public float finalLength = 0.02f;
+    public float initialLength = 0.02f;
+
+    private Vector3 positionLaser;
+    private LineRenderer lineRenderer;
+    private float distance = 0;
+	// Use this for initialization
+	void Start () {
+        distance = distanceLaser;
+        positionLaser = new Vector3(0, 0, finalLength);
+        lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+        lineRenderer.startColor = laserColor;
+        lineRenderer.endColor = laserColor;
+        lineRenderer.startWidth = initialLength;
+        lineRenderer.endWidth = finalLength;
+        lineRenderer.positionCount = 2;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, distanceLaser))
+        {
+            distance = hit.distance;
+            lineRenderer.SetPosition(0, transform.position);
+            lineRenderer.SetPosition(1, hit.point);
+        }
+        else
+        {
+            distance = distanceLaser;
+            lineRenderer.SetPosition(0, transform.position);
+            lineRenderer.SetPosition(1, transform.position + transform.forward * distanceLaser);
+        }
+    }
+
+    public float getDistance()
+    {
+   
+        return distance/distanceLaser;
+    }
+}
