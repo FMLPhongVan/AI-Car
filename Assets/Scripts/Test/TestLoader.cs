4698da5a4cb8e978f5017cc12e20576eb53d64d8
PhongVan <fmlphongvan@gmail.com>
2022-12-07 23:56:20 +0700

    Upload follow camera

---
+++ Assets/Scripts/Test/TestLoader.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLoader : MonoBehaviour
+{
+    public string Model = "";
+    public GameObject TestCarVariant;
+    public Vector3 StartPosition;
+
+    void Awake()
+    {
+        Time.timeScale = 2f;
+        GameObject car = Instantiate(TestCarVariant, StartPosition, Quaternion.identity);
+        car.GetComponent<TesterAIController>().Network = new NeuralNetwork("./records/" + Model + ".txt");
+    }
+}
